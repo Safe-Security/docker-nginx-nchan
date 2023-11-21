17259d3338be399d4e6af63c15cf01e3c0c75e1f
Mike Preston <mike@technomonk.com>
2023-11-21 04:58:08 +0000

    update base images

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 # Desc: Build script for nginx-nchan
 # build for amd64, arm64
-docker buildx build --platform linux/amd64,linux/arm64 -t darkflib/nginx-nchan:latest -t darkflib/nginx-nchan:1.24.0 --push .
+docker buildx build --pull --platform linux/amd64,linux/arm64 -t darkflib/nginx-nchan:latest -t darkflib/nginx-nchan:1.25 -t darkflib/nginx-nchan:1.25.3 --push .
